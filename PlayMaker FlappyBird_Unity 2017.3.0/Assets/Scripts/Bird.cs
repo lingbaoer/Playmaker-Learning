8862f547081dfd9860965bff1e3601eb7e681c3b
yangyaobin <yangyaobin@3dov.cn>
2018-03-28 13:18:13 +0800

    添加项目

---
+++ PlayMaker FlappyBird_Unity 2017.3.0/Assets/Scripts/Bird.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2d;
+    private PlayMakerFSM fsm;
+
+    private void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        fsm = GetComponent<PlayMakerFSM>();
+    }
+
+    public void AddForce(Vector2 force)
+    {
+        Vector2 velocity = rigidbody2d.velocity;
+        velocity.y = 0;
+        rigidbody2d.velocity = velocity;
+        rigidbody2d.AddForce(force);
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        fsm.SendEvent("GameOver");
+    }
+}
